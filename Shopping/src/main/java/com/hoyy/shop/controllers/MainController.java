7acd8ff982b64743df71584282d07e02150d5489
HoYY <hoyong8532@naver.com>
2020-02-19 19:39:25 +0900

    hello world

---
+++ Shopping/src/main/java/com/hoyy/shop/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.hoyy.shop.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@GetMapping
+	public String mainPage() {
+		return "hello";
+	}
+}
